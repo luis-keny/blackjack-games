9211235f33cac72140ba3a40e61c5d119c679a45
luis-keny <luis.lucero03@outlook.com>
2025-12-10 12:54:13 -0500

    refactor: enhance Desktop component props structure and improve button accessibility

--- src/blackjack-game/components/Desktop.tsx
+++ src/blackjack-game/components/Desktop.tsx
@@ -2,12 +2,21 @@ interface DesktopProps {
   playerName: string
   score?: number
   hasActions?: boolean
+  isDisabledActions?: boolean
   onAdd?: () => void
   onReady?: () => void
   playerHand: string[]
 }
 
-export const Desktop = ({ playerName, score, hasActions = false, onAdd, onReady, playerHand = [] }: DesktopProps) => {
+export const Desktop = ({ 
+  playerName,
+  score,
+  hasActions = false,
+  isDisabledActions = false,
+  onAdd,
+  onReady,
+  playerHand = []
+}: DesktopProps) => {
   return (
     <article>
       <header className="flex justify-between items-center">
@@ -19,17 +28,19 @@ export const Desktop = ({ playerName, score, hasActions = false, onAdd, onReady,
           </span>
         </h2>
         {hasActions && (<div className="space-x-2">
-          <button 
+          <button
+            disabled={isDisabledActions} 
             onClick={onAdd}
             className="btn-player-add-card bg-cyan-500 px-4 py-1 rounded-md h-fit cursor-pointer ring-cyan-500/50 hover:ring-4 active:scale-95 disabled:opacity-50"
           >Add</button>
           <button 
+            disabled={isDisabledActions}
             onClick={onReady}
             className="btn-player-ready bg-blue-600 px-4 py-1 rounded-md h-fit cursor-pointer ring-blue-500/50 hover:ring-4 active:scale-95 disabled:opacity-50">
           Ready</button>
         </div>)}
       </header>
-      <section className="player-desktop flex px-6 pl-38 py-4 bg-slate-800 rounded-md h-[352px] overflow-x-auto">
+      <section className="player-desktop flex px-6 pl-38 py-4 bg-slate-800 rounded-md h-[352px] overflow-x-auto overflow-y-hidden">
         {playerHand.length > 0 && playerHand.map(card => (
           <img key={`card-player-${playerName}-${card}`} src={card} className="rounded-lg h-80 w-52 -ml-32" alt={card}></img>
         ))}
