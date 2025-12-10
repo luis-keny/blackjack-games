ca86ac8e5139bff21fc01ff63d0e42479b4016a5
luis-keny <luis.lucero03@outlook.com>
2025-12-10 12:54:39 -0500

    feat: update App component to integrate useGame hook and improve dealer hand management

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,42 @@
 import { HeaderCustom } from "./shared/components/HeaderCustom"
 import { WinnerAnnouncement } from "./shared/components/WinnerAnnouncement"
 import { Desktop } from "./blackjack-game/components/Desktop.tsx"
+import { useGame } from "./blackjack-game/hooks/useGame.tsx"
 
-const dealerHandInit = [
-  '/img/grey_back.png',
+const dealerHiddenHand = [
+  '/img/red_back.png',
   '/img/grey_back.png',
 ]
 
 export const App = () => {
+  const {
+    dealerHand,
+    playerHand,
+    winner,
+    dealerScore,
+    playerScore,
+    isReadyPlayer,
+    handlePlayPlayer,
+    handleReadyPlayer,
+    handleResetGame,
+  } = useGame()
+
   return (
     <>
-      <HeaderCustom title="BlackJack" hasActions/>
-      <WinnerAnnouncement />
+      <HeaderCustom title="BlackJack" hasActions onNewGame={handleResetGame}/>
+      <WinnerAnnouncement winner={winner} />
 
       <main className="max-w-[800px] mx-auto space-y-4">
-        <Desktop playerName="Device" playerHand={dealerHandInit}/>
-        <Desktop playerName="Player" hasActions score={0} playerHand={[]}/>
+        <Desktop playerName="Dealer" score={dealerScore} playerHand={isReadyPlayer ? dealerHand : dealerHiddenHand}/>
+        <Desktop 
+          playerName="Player" 
+          hasActions 
+          score={playerScore} 
+          playerHand={playerHand} 
+          onAdd={handlePlayPlayer} 
+          onReady={handleReadyPlayer}
+          isDisabledActions={isReadyPlayer}
+        />
       </main>
     </>
   )
